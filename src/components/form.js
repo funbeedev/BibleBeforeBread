a29a4c077da74c7daefb1d6dfbc195da18c5f0e5
Fum <66768334+funbeedev@users.noreply.github.com>
2021-11-20 10:17:13 +0000

    revert form.js

--- src/components/form.js
+++ src/components/form.js
@@ -6,19 +6,19 @@ const Form = () => (
 
     <form name="subscribe" method="POST" data-netlify="true">
 
-      <p className="coming-soon"> Daily Bible Inspirations Coming Soon. </p>
-      <p className="enter-email"> Enter your email to stay informed. </p>
+      <p class="coming-soon"> Daily Bible Inspirations Coming Soon. </p>
+      <p class="enter-email"> Enter your email to stay informed. </p>
 
       <input type="hidden" name="form-name" value="subscribe" />
 
-      <div className="fields">
-        <div className="email-field">
+      <div class="fields">
+        <div class="email-field">
           <input type="email" name="email" id="email" placeholder="example@email.com"/>
           <label for="email" htmlFor="email">Your Email </label>
         </div> 
 
-        <button className="home-button" type="submit">
-          <span className="button-text">Send</span>
+        <button class="home-button" type="submit">
+          <span class="button-text">Send</span>
           <img src={arrow} className="button-arrow" alt="Arrow" />
         </button>
       </div>
