85fed3a6eb4f7e118af4f14176bfd8f9040e3796
Fum <66768334+funbeedev@users.noreply.github.com>
2021-11-20 10:18:18 +0000

    revert index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,32 +10,32 @@ import VantaCloudsAnimation from "../components/vanta-animation"
 import "../style/index.scss"
 
 const Home = () => (
+
   <Layout>
-    <div className="home-logo" />
+
+    <div class="home-logo" />
 
     {/* add vanta animation, is helmet needed? */}
     <Helmet>
-      <title>Bible Before Bread</title>
-      <script src="../../static/three.r119.min.js"></script>
-      <script src="../../static/vanta.clouds.min.js"></script>
-    </Helmet>
+        <script src="../../static/three.r119.min.js"></script>
+        <script src="../../static/vanta.clouds.min.js"></script>
+    </Helmet>      
     <VantaCloudsAnimation>
-      <div className="home-quote">
-        <blockquote>
-          {" "}
-          "Man shall not live by bread alone, but by every word that comes from
-          the mouth of God."
-        </blockquote>
-        <img src={line} className="quote-line" alt="Logo" />
-        <p className="quote-verse">- Matthew 4:4</p>
-      </div>
+        <div class="home-quote">
+          <blockquote> "Man shall not live by bread alone, but by every word that comes from the mouth of God."</blockquote>
+          <img src={line} className="quote-line" alt="Logo" />
+          <p class="quote-verse">- Matthew 4:4</p>
+        </div>  
     </VantaCloudsAnimation>
 
     <div className="form-container">
       <Form />
-      <img className="form-image" src={form_image} alt="" />
+      <img class="form-image" src={form_image} alt="" />
     </div>
+
   </Layout>
+  
+  
 )
 
 export default Home
